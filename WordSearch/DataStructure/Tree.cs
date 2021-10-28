eaaa25107b05228a6eb3e139e5451bd09d832b6b
Emil Örjes <emil.orjes@hotmail.com>
2021-10-28 16:00:17 +0200

    Added comments to Tree class

--- WordSearch/DataStructure/Tree.cs
+++ WordSearch/DataStructure/Tree.cs
@@ -6,6 +6,14 @@ namespace WordSearch.DataStructure
     {
         private Node root;     
 
+        /// <summary>
+        /// Tar in ett sökord och en tupel med resultat.
+        /// Om roten inte är lika med null så kallar den på Insert metoden i Node klassen
+        /// och skickar med sökordet och resultatet.
+        /// Annars sätts en ny node men sökordet och resultatet.
+        /// </summary>
+        /// <param name="searchword">Sökordet</param>
+        /// <param name="result">En tuple med resultatet</param>
         public void Insert(string searchword, Tuple<string, int>[] result)
         {
             if (root != null)
@@ -18,6 +26,10 @@ namespace WordSearch.DataStructure
             }
         }
      
+        /// <summary>
+        /// Om roten inte är lika med null kallar den på PrintNodes metoden i Node klassen.
+        /// Annars skrivs det ut att det inte finns något träd än (ingen rot node är satt ännu).
+        /// </summary>
         public void PrintTree()
         {
             if (root != null)
